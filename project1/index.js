254e256857d62b4b9777aebc40a7158a273dda4f
hukai <huzhengen@163.com>
2019-09-29 17:26:14 +0800

    Update index.js

--- project1/index.js
+++ project1/index.js
@@ -4,7 +4,10 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import HelloWrold from './HelloWorldApp';
+import HelloWorldApp from './HelloWorldApp';
+import Bananas from './Bananas';
+import BlinkApp from './BlinkApp';
+import LotsOfGreetings from './LotsOfGreetings';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => HelloWrold);
+AppRegistry.registerComponent(appName, () => LotsOfGreetings);
