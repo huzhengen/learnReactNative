fcebd9b48130752a8bee740120c5563626d930da
hukai <huzhengen@163.com>
2019-09-29 17:36:23 +0800

    Update index.js

--- project1/index.js
+++ project1/index.js
@@ -7,7 +7,9 @@ import App from './App';
 import HelloWorldApp from './HelloWorldApp';
 import Bananas from './Bananas';
 import BlinkApp from './BlinkApp';
+import LotsOfStyles from './LotsOfStyles';
+import FixedDimensionsBasics from './FixedDimensionsBasics';
 import LotsOfGreetings from './LotsOfGreetings';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => LotsOfGreetings);
+AppRegistry.registerComponent(appName, () => FixedDimensionsBasics);
