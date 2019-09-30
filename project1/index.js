9c817373cf0c3df37c72497fc233dc5edd1a6fcd
hukai <huzhengen@163.com>
2019-09-30 10:45:44 +0800

    Update index.js

--- project1/index.js
+++ project1/index.js
@@ -11,12 +11,12 @@ import LotsOfStyles from './LotsOfStyles';
 import FlexDimensionsBasics from './FlexDimensionsBasics';
 import FixedDimensionsBasics from './FixedDimensionsBasics';
 import PizzaTranslator from './PizzaTranslator';
-
+import ButtonBasics from './ButtonBasics';
+import Touchables from './Touchables';
 
 import FlexDirectionBasics from './FlexDirectionBasics';
-
 
 import LotsOfGreetings from './LotsOfGreetings';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => PizzaTranslator);
+AppRegistry.registerComponent(appName, () => Touchables);
